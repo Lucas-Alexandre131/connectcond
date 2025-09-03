eeb4b2ee8a9a287c5abaf2431caea5062ff49e3d
Lucas-Alexandre131 <lucasalexandredeoliveira329@gmail.com>
2025-09-03 18:28:18 -0300

    criada a função base para a verificação

--- js/cookie/verification.js
+++ js/cookie/verification.js
@@ -1,1 +1,7 @@
+import { getCookie } from "./authCookie.js";
+
+
 // função responsável por verificar o token de acesso ao carregar a pagina html
+export function verificationToken(token) {
+    
+}
